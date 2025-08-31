8b03974bcdbf0af2b9c7cf314c505c7c500a2127
bduck22 <hwangwb1224@hanmail.net>
2025-08-31 15:26:26 +0900

    3

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -32,13 +32,66 @@ public class Enemy : MonoBehaviour
     public float Speed;
 
     public float Damage;
+
+    public bool stun;
+
+    public Character Target;
     void Start()
     {
-        
+        InvokeRepeating("LoadTarget", 0.25f, 0.25f);
     }
+
+    public float time;
+
+    public bool isAttack;
+
+    public float Range;
 
     void Update()
     {
-        
+        if (!stun) 
+        {
+            if (!isAttack&&time >= 1)
+            {
+                isAttack = true;
+            }
+            else if(time < 1)
+            {
+                time += Time.deltaTime;
+            }
+            if (Target)
+            {
+                transform.LookAt(Target.transform);
+                if(Vector3.Distance(transform.position, Target.transform.position) <= Range)
+                {
+                    if (isAttack)
+                    {
+                        isAttack = false;
+                        time = 0;
+                        Attack();
+                    }
+                }
+                else transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Speed*Time.deltaTime);   
+            }
+        }
+    }
+    void LoadTarget()
+    {
+        foreach(Character c in GameManager.Instance.CameraController.characters)
+        {
+            if (Target)
+            {
+                if (Vector3.Distance(transform.position, c.transform.position) < Vector3.Distance(transform.position, Target.transform.position))
+                {
+                    Target = c;
+                }
+            }
+            else Target = c;
+        }
+    }
+
+    public void Attack()
+    {
+
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public CameraController CameraController;
     void Start()
     {
         Instance = this;
